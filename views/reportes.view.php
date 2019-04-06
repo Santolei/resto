096fc5728ebd92f74aa6194cffbec22107ffc641
Santolei <s.tolosa1@gmail.com>
2019-04-06 13:00:30 -0300

    Datetimepicker para reportes

--- views/reportes.view.php
+++ views/reportes.view.php
@@ -11,6 +11,8 @@
         
         <!-- Sidebar -->
         <?php require_once 'sidebar.php'; ?>
+        <!-- Tempus Dominus datetime-picker css -->
+        <link rel="stylesheet" href="public/css/addons/tempusdominus-bootstrap-4.min.css" />
 
     </header>
     <!--Main Navigation-->
@@ -21,27 +23,48 @@
             <div class="container pt-2">
                 <div class="card mt-4 mb-4 d-flex justify-content-center text-center">
                     <h4 class="m-4">
-                        <strong class="titulo-seccion">Reportes</strong>
+                        <strong class="titulo-seccion">Reporte personalizado</strong>
                     </h4>
                 </div>
-
-                <div class="row">
-                    <div class="col-12">
-                        <div class="card p-4">
-                            <h4 class="text-center titulo-seccion">Últimas ventas</h4>
-
-                            <table style="width:100%; border-radius: 6px" class="display table table-striped table-sm" id="tabla_ingresos">
-                                <thead class="tabla-thead white-text">
-                                    <th>Detalle</th>
-                                    <th>Fecha</th>
-                                    <th>Monto</th>
-                                    <th>Método de pago</th>
-                                </thead>
-                                <tbody></tbody>
-                            </table>
-                        </div>                        
-                    </div>
-                </div>
+                    
+                <div class="card p-2">
+                    <h5 class="titulo-seccion text-center">Reporte de ventas por fecha</h5>
+                    <form action="consultas/reporte-fecha.php" method="POST">
+                        <div class="row">
+                            <!-- DESDE -->
+                            <div class="col-12 col-lg-6">
+                                <div class="form-group">
+                                    <label for="desde">Fecha de inicio</label>
+                                    <div class="input-group date" id="desde" data-target-input="nearest">
+                                        <input type="text" name="desde" class="form-control datetimepicker-input" data-target="#desde"/>
+                                        <div class="input-group-append" data-target="#desde" data-toggle="datetimepicker">
+                                            <div class="input-group-text"><i class="fa fa-calendar"></i></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        
+                            <!-- HASTA -->
+                            <div class="col-12 col-lg-6">
+                                <div class="form-group">
+                                    <label for="hasta">Fecha final</label>
+                                    <div class="input-group date" id="hasta" data-target-input="nearest">
+                                        <input type="text" name="hasta" class="form-control datetimepicker-input" data-target="#hasta"/>
+                                        <div class="input-group-append" data-target="#hasta" data-toggle="datetimepicker">
+                                            <div class="input-group-text"><i class="fa fa-calendar"></i></div>
+                                        </div>
+                                    </div>
+                                </div>                
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-12 text-center">
+                                <button type="submit" class="btn btn-primary" name="submit">Generar reporte</button>
+                            </div>
+                        </div>
+                    </form>
+                </div> 
+                    
             </div>
             
         </main>
@@ -50,43 +73,32 @@
     
     <!-- Footer -->
     <?php require_once 'footer.php';?>
-    <!-- MDBootstrap Datatables  -->
-    <script type="text/javascript" src="public/js/addons/datatables.min.js"></script>
-    <script type="text/javascript" src="public/js/addons/dataTables.altEditor.js"></script>
+    <!-- Datetime picker -->
+    <script type="text/javascript" src="public/js/moment-with-locales.js"></script>
+    <script type="text/javascript" src="public/js/tempusdominus-bootstrap-4.min.js"></script>
+
     <style type="text/css">
         .footer-copyright {
             display: none;
         }
     </style>
-    <script>
-            
-        $(document).ready(function () {
-          var tbl = $('#tabla_ingresos').DataTable({
-            "processing": false,
-            "serverSide": true,
-            autoFill: true,
-            "ajax": "consultas/caja_json.php",
-            "order": [[ 0, 'desc' ], [ 1, 'asc' ]],
-            "searching": false,
-            "fnRowCallback": function( nRow, aData, iDisplayIndex ) {
-            $('td:eq(2)', nRow).html('$' + aData[2]);
-            return nRow;
-            },
-            "fnRowCallback": function( nRow, aData, iDisplayIndex ) {
-            $('td:eq(0)', nRow).html('<a href="detalle_venta.php?id=' + aData[0] + '">' +
-                'Ver detalle' + '</a>');
-            return nRow;
-            },
-            "language": {
-              "url": "public/js/addons/datatablesspanish.json"
-            }
-          });
-
-          $('.dataTables_length').addClass('bs-select');
+    <script type="text/javascript">
+        $(function () {
+            $('#desde').datetimepicker({
+                viewMode: 'years',
+                locale: 'es',
+                icons: {
+                    up: "fa fa-arrow-up blue-text shadow-none",
+                    down: "fa fa-arrow-down blue-text"
+                }
+            });
+            $('#hasta').datetimepicker({
+                viewMode: 'years',
+                locale: 'es'
+            });
         });
-
-
     </script>
+    
 
     
     
